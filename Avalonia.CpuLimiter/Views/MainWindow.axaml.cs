5bcd6618e20984b6bae6d7641e5863cd35a647d5
hiddenblue <freewayrong@foxmail.com>
2024-11-02 23:10:24 +0800

    Feature: add Exit request message and dependency

--- Avalonia.CpuLimiter/Views/MainWindow.axaml.cs
+++ Avalonia.CpuLimiter/Views/MainWindow.axaml.cs
@@ -1,6 +1,12 @@
+using System;
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using Avalonia.CpuLimiter.ViewModels;
+using Avalonia.Interactivity;
 using Avalonia.ReactiveUI;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Dto;
+using MsBox.Avalonia.Enums;
 
 namespace Avalonia.CpuLimiter.Views
 {
@@ -14,6 +20,34 @@ namespace Avalonia.CpuLimiter.Views
         public static async Task DoOpenAboutWindowAsync()
         {
             var aboutWindow = AboutWindow.GetInstance();
+            aboutWindow.Show();
+        }
+
+
+        private async void OnExitButtonClicked(object? sender, RoutedEventArgs e)
+        {
+            var box = MessageBoxManager
+                .GetMessageBoxStandard(new MessageBoxStandardParams()
+                {
+                    Width = 300,
+                    Height = 120,
+                    ContentTitle = "Exit",
+                    ContentMessage = "Are you sure to exit?",
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                    ButtonDefinitions = ButtonEnum.YesNo,
+                    Icon = MsBox.Avalonia.Enums.Icon.Question
+                });
+
+            ButtonResult result = await box.ShowWindowDialogAsync(this);
+            
+            if(result == ButtonResult.Yes) 
+                Environment.Exit(0);
+
+        }
+
+        private async void OnOpenAboutWindowClicked(object? sender, RoutedEventArgs e)
+        {
+            var aboutWindow = new AboutWindow();
             aboutWindow.Show();
         }
     }
