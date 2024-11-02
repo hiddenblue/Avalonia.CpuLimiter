500c842661f9bafa7ce891e90b1d4f6330f04fb2
hiddenblue <freewayrong@foxmail.com>
2024-11-02 23:08:18 +0800

    improve: simplified the code of about window

--- Avalonia.CpuLimiter/Views/AboutWindow.axaml.cs
+++ Avalonia.CpuLimiter/Views/AboutWindow.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Avalonia.CpuLimiter.Views;
@@ -19,5 +20,10 @@ public partial class AboutWindow : Window
     {
         Instance = new AboutWindow();
         return Instance;
+    }
+
+    private async void OnCloseButtonClick(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
     }
 }
