4f43870958b679eeb9a02398f0968275f7b87047
hiddenblue <62304226+hiddenblue@users.noreply.github.com>
2024-11-10 17:50:16 +0800

    Feature:  Add global theme color switching

--- Avalonia.CpuLimiter/ViewModels/SettingWindowViewModel.cs
+++ Avalonia.CpuLimiter/ViewModels/SettingWindowViewModel.cs
@@ -1,7 +1,21 @@
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Avalonia.Media;
 using ReactiveUI;
 
 namespace Avalonia.CpuLimiter.ViewModels;
+
+public class CustomColor
+{
+    public string Hex { get; }
+    public Color Color { get; }
+
+    public CustomColor(string hex)
+    {
+        Hex = hex;
+        Color = Color.Parse(hex);
+    }
+}
 
 public enum StartupDecoration
 {
@@ -15,6 +29,8 @@ public class SettingWindowViewModel : ViewModelBase
     public SettingWindowViewModel()
     {
         HistoryLimit = 5;
+        // this.WhenAnyValue(x => x._colorDigital)
+        //     .Subscribe()
 
 
     }
@@ -42,6 +58,43 @@ public class SettingWindowViewModel : ViewModelBase
         get => _isDarkTheme;
         set => this.RaiseAndSetIfChanged(ref _isDarkTheme, value);
     }
+    
+    private int _colorDigital = 4;
+
+    public int ColorDigital
+    {
+        get => _colorDigital;
+        set => this.RaiseAndSetIfChanged(ref _colorDigital, value);
+    }
+    
+    public ObservableCollection<CustomColor> ColorCollection { get; } = new ObservableCollection<CustomColor>
+    {
+        new CustomColor("#e95815"),
+        new CustomColor("#f1a100"),
+        new CustomColor("#f0c400"),
+        new CustomColor("#e7e542"),
+        
+        new CustomColor("#bbd53e"),
+        new CustomColor("#4fb142"),
+        new CustomColor("#068bce"),
+        // new CustomColor("#014da1"),
+        
+        // new CustomColor("#192c92"),
+        // new CustomColor("#522a8b"),
+        new CustomColor("#b01e4f"),
+        new CustomColor("#e83a17"),
+        
+        new CustomColor(Colors.Silver.ToString()),
+        new CustomColor(Colors.Gray.ToString()),
+        new CustomColor(Colors.Black.ToString()),
+        new CustomColor(Colors.Aqua.ToString()),
+        new CustomColor(Colors.SkyBlue.ToString()),
+        new CustomColor(Colors.DeepSkyBlue.ToString()),
+        new CustomColor(Colors.LightSkyBlue.ToString())
+    };
+
+
+
 
     
 }
--- Avalonia.CpuLimiter/Views/SettingWindow.axaml.cs
+++ Avalonia.CpuLimiter/Views/SettingWindow.axaml.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.CpuLimiter.ViewModels;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using Avalonia.Styling;
+using ReactiveUI;
 
 namespace Avalonia.CpuLimiter.Views;
 
@@ -15,5 +22,54 @@ public partial class SettingWindow : Window
         {
             Border.Material.TintColor = Colors.SkyBlue;
         }
+
+        // this.WhenAnyValue(x => x.ColorSlider.Value)
+        //     .Subscribe(RefreshThemeColor);
     }
+
+
+    private void OnRefreshThemeColor(object sender, RangeBaseValueChangedEventArgs e)
+    {
+        int colorIndex = (int)e.NewValue;
+
+        Border.Material.TintColor = CustomColors[colorIndex].Color;
+    }
+
+
+    private List<CustomColor> CustomColors = new List<CustomColor>()
+    {
+        new CustomColor("#e95815"),
+        new CustomColor("#f1a100"),
+        new CustomColor("#f0c400"),
+        new CustomColor("#e7e542"),
+
+        new CustomColor("#bbd53e"),
+        new CustomColor("#4fb142"),
+        new CustomColor("#068bce"),
+        // new CustomColor("#014da1"),
+
+        // new CustomColor("#192c92"),
+        // new CustomColor("#522a8b"),
+        new CustomColor("#b01e4f"),
+        new CustomColor("#e83a17"),
+
+        new CustomColor(Colors.Silver.ToString()),
+        new CustomColor(Colors.Gray.ToString()),
+        new CustomColor(Colors.Black.ToString()),
+        new CustomColor(Colors.Aqua.ToString()),
+        new CustomColor(Colors.SkyBlue.ToString()),
+        new CustomColor(Colors.DeepSkyBlue.ToString()),
+        new CustomColor(Colors.LightSkyBlue.ToString())
+    };
+    
+    private void OnToggleThemeButtonClicked(object? sender, RoutedEventArgs e)
+    {
+        if (RequestedThemeVariant == ThemeVariant.Dark)
+            RequestedThemeVariant = ThemeVariant.Light;
+        else
+        {
+            RequestedThemeVariant = ThemeVariant.Dark;
+        }
+    }
+
 }
