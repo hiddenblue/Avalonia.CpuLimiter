e970b4116c5bdd758f09ccbe7434665fce548d05
hiddenblue <62304226+hiddenblue@users.noreply.github.com>
2024-11-15 03:09:14 +0800

    Fix: fix the config load problem, remove color

--- Avalonia.CpuLimiter/Models/ConfigModel.cs
+++ Avalonia.CpuLimiter/Models/ConfigModel.cs
@@ -1,33 +1,57 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Avalonia.CpuLimiter.ViewModels;
+using Avalonia.Media;
 using Avalonia.Styling;
 
 namespace Avalonia.CpuLimiter.Models;
+
+
+// pay attention to the JsonElement. the dynamic type need it
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(MyConfigModel))]
+[JsonSerializable(typeof(ThemeVariant))]
+[JsonSerializable(typeof(CustomColor))]
+[JsonSerializable(typeof(JsonElement))]
+[JsonSerializable(typeof(Color))]
+[JsonSerializable(typeof(string))]
+internal partial class MyConfigModelContext : JsonSerializerContext
+{
+   
+}
 
 public class MyConfigModel
 {
    public MyConfigModel()
    {
+      
    }
 
    public MyConfigModel(MyConfigModel myConfig)
    {
       HistoryLimit = myConfig.HistoryLimit;
-      UserColor = myConfig.UserColor;
+      ColorIndex = myConfig.ColorIndex;
       StartupDecorationConfig = myConfig.StartupDecorationConfig;
       StartupCultureConfig = myConfig.StartupCultureConfig;
       ThemeVariantConfig = myConfig.ThemeVariantConfig;
+      aboutInfo = new AboutInfo();
    }
 
-   public int HistoryLimit;
-   public CustomColor UserColor;
-   public StartupDecoration StartupDecorationConfig;
-   public string StartupCultureConfig;
-   public  ThemeVariant ThemeVariantConfig;
+   public int HistoryLimit { get; set; }
+   
+   public int ColorIndex { get; set; }
 
-   public AboutInfo AboutInfo;
+   public StartupDecoration StartupDecorationConfig  { get; set; }
+   
+   public string StartupCultureConfig  { get; set; }
+   
+   public  ThemeVariant ThemeVariantConfig  { get; set; }
 
-   public Collection<CustomColor> CustomColorCollection;
+   public AboutInfo aboutInfo { get; set; }
+
+   // public Collection<CustomColor> CustomColorCollection { get; set; }
 
 
 
@@ -36,12 +60,12 @@ public class MyConfigModel
 
 public class AboutInfo
 {
-   public static readonly string AppName = "Avalonia.CpuLimiter";
-   public static readonly string AppVersion = "1.0";
-   public static readonly string Author = "Hiddenblue";
-   public static readonly string DotnetVersion = "8.0";
-   public static readonly string AvaloniaUiVersion = "11.20";
-   public static readonly string License = "GPLV2";
+   public static  string AppName { get; }= "Avalonia.CpuLimiter";
+   public static  string AppVersion { get; }= "1.0";
+   public static  string Author { get; }= "Hiddenblue";
+   public static  string DotnetVersion { get; } = "8.0";
+   public static  string AvaloniaUiVersion { get; }= "11.20";
+   public static  string License  { get;} = "GPL-2.0";
 }
 
 public enum StartupDecoration
--- Avalonia.CpuLimiter/Services/ConfigFileService.cs
+++ Avalonia.CpuLimiter/Services/ConfigFileService.cs
@@ -3,12 +3,11 @@ using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Avalonia.CpuLimiter.Models;
-using Avalonia.CpuLimiter.Services;
 using Avalonia.CpuLimiter.ViewModels;
 using Avalonia.Media;
 using Avalonia.Styling;
 
-namespace Avalonia.CpuLimiter.Config;
+namespace Avalonia.CpuLimiter.Services;
 
 public class ConfigFileService
 {
@@ -23,51 +22,57 @@ public class ConfigFileService
     {
         return new MyConfigModel()
         {
-            AboutInfo = new AboutInfo(),
+            aboutInfo = new AboutInfo(),
             HistoryLimit = 5,
-            UserColor = new CustomColor(Colors.Black),
+            ColorIndex = 0,
             StartupDecorationConfig = StartupDecoration.Fluent,
             // default culture is empty.
             StartupCultureConfig =  "",
-            ThemeVariantConfig = ThemeVariant.Default
+            ThemeVariantConfig = ThemeVariant.Dark
         };
     }
 
     public static async Task SaveConfigAsync(MyConfigModel configModel)
     {
-       if(!Directory.Exists(Path.GetDirectoryName(configPath)))
-           Directory.CreateDirectory(path: Path.GetDirectoryName(configPath) ?? throw new InvalidOperationException());
+        if (!Directory.Exists(Path.GetDirectoryName(configPath)))
+        {
+            Directory.CreateDirectory(path: Path.GetDirectoryName(configPath) ?? throw new InvalidOperationException());
+        }
 
-       await using (FileStream fs = File.Open(configPath, FileMode.Create))
-       {
-           await JsonSerializer.SerializeAsync<MyConfigModel>(fs, configModel, SourceGenerationContext.Default.MyConfigModel);
-       }
+        string oldConfig;
+        
+        if (File.Exists(configPath))
+        {
+            // backup the previous config
+            oldConfig = await File.ReadAllTextAsync(configPath);
+        }
+        else
+        {
+            Console.WriteLine("try to save a config file, but it doesn't exist.");
+            oldConfig = string.Empty;
+        }
+        
+        try
+        {
+            await using (FileStream fs = File.Open(configPath, FileMode.Create))
+            {
+                await JsonSerializer.SerializeAsync(fs, configModel, MyConfigModelContext.Default.MyConfigModel);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            // when code above not working, recover the old config
+            await File.WriteAllTextAsync(configPath, oldConfig);
+            throw;
+        }
+
+
+
+
     }
 
-    // public static async Task<MyConfigModel> LoadConfigAsync()
-    // {
-    //     try
-    //     {
-    //         if (Path.Exists(configPath))
-    //         {
-    //             await using (FileStream fs = File.Open(configPath, FileMode.Open))
-    //             {
-    //                 return (await JsonSerializer.DeserializeAsync<MyConfigModel>(fs, SourceGenerationContext.Default.MyConfigModel))!;
-    //             }
-    //         }
-    //         Console.WriteLine(@"No config file found, using default config");
-    //         return await GetDefaultConfigModel();
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         Console.WriteLine(e);
-    //         Console.WriteLine(@"Failed to load config file exception");
-    //         throw;
-    //     }
-    // }
-    
-    
-    public static MyConfigModel LoadConfigAsync()
+    public static async Task<MyConfigModel> LoadConfigAsync()
     {
         try
         {
@@ -75,11 +80,112 @@ public class ConfigFileService
             {
                 using (FileStream fs = File.Open(configPath, FileMode.Open))
                 {
-                    return ( JsonSerializer.Deserialize<MyConfigModel>(fs, SourceGenerationContext.Default.MyConfigModel))!;
+                    MyConfigModel configModel =
+                        JsonSerializer.Deserialize(fs,
+                            MyConfigModelContext.Default.MyConfigModel);
+                    if (configModel != null)
+                    {
+                        // convert the Themevalue read from json
+                        if(configModel.ThemeVariantConfig.Key.ToString() == "Dark")
+                            configModel.ThemeVariantConfig = ThemeVariant.Dark;
+                        else if(configModel.ThemeVariantConfig.Key.ToString() == "Light")
+                            configModel.ThemeVariantConfig = ThemeVariant.Light;
+                        else
+                        {
+                            configModel.ThemeVariantConfig = ThemeVariant.Default;
+                        }
+                        return configModel;
+                    }
+                    else
+                        throw new NullReferenceException("the file content was null");
+
                 }
             }
+
+            throw new FileNotFoundException("The config file was not found.");
+        }
+        catch (System.Text.Json.JsonException)
+        {
+            Console.WriteLine("Failed to load config file");
+            SaveConfigAsync(GetDefaultConfigModel());
+            Console.WriteLine($"Create a default config file");
+            return GetDefaultConfigModel();
+        }
+        catch (FileNotFoundException)
+        {
+            SaveConfigAsync(GetDefaultConfigModel());
+            Console.WriteLine($"Create a default config file");
             Console.WriteLine(@"No config file found, using default config");
             return GetDefaultConfigModel();
+        }
+        catch (NullReferenceException)
+        {
+            SaveConfigAsync(GetDefaultConfigModel());
+            Console.WriteLine($"The config file could not be loaded");
+            Console.WriteLine(@"No config file found, using default config");
+            return GetDefaultConfigModel(); 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Console.WriteLine(@"Failed to load config file exception");
+            throw;
+        }
+    }
+    
+    
+    public static MyConfigModel LoadConfig()
+    {
+        try
+        {
+            if (Path.Exists(configPath))
+            {
+                using (FileStream fs = File.Open(configPath, FileMode.Open))
+                {
+                    MyConfigModel configModel =
+                        JsonSerializer.Deserialize(fs,
+                            MyConfigModelContext.Default.MyConfigModel);
+                    if (configModel != null)
+                    {
+                        // convert the Themevalue read from json
+                        if(configModel.ThemeVariantConfig.Key.ToString() == "Dark")
+                            configModel.ThemeVariantConfig = ThemeVariant.Dark;
+                        else if(configModel.ThemeVariantConfig.Key.ToString() == "Light")
+                            configModel.ThemeVariantConfig = ThemeVariant.Light;
+                        else
+                        {
+                            configModel.ThemeVariantConfig = ThemeVariant.Default;
+                        }
+                        return configModel;
+                    }
+                    else
+                        throw new NullReferenceException("the file content was null");
+
+                }
+            }
+
+            throw new FileNotFoundException("The config file was not found.");
+        }
+        catch (System.Text.Json.JsonException)
+        {
+            Console.WriteLine("Failed to load config file");
+            SaveConfigAsync(GetDefaultConfigModel());
+            Console.WriteLine($"Create a default config file");
+            return GetDefaultConfigModel();
+        }
+        catch (FileNotFoundException)
+        {
+            SaveConfigAsync(GetDefaultConfigModel());
+            Console.WriteLine($"Create a default config file");
+            Console.WriteLine(@"No config file found, using default config");
+            return GetDefaultConfigModel();
+        }
+        catch (NullReferenceException)
+        {
+            SaveConfigAsync(GetDefaultConfigModel());
+            Console.WriteLine($"The config file could not be loaded");
+            Console.WriteLine(@"No config file found, using default config");
+            return GetDefaultConfigModel(); 
         }
         catch (Exception e)
         {
