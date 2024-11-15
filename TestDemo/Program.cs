0c916ae20059da2dcf20aa0e8cb7275075cf29a0
hiddenblue <freewayrong@foxmail.com>
2024-11-15 19:26:01 +0800

    Test: add test demo that print cpu used

---
+++ TestDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Avalonia.CpuLimiter.Models;
+
+namespace TestDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (TextWriter tw = new StreamWriter("output.txt"))
+        {
+            Console.SetOut(tw);
+         
+        Process process = Process.GetCurrentProcess();
+
+        Console.WriteLine(ProcessHelper.GetProcessAffinityBitMask(process));
+           
+        }
+        return;
+
+    }
+}
