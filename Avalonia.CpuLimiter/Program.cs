d978e606d508393671290940324736cc4972339a
hiddenblue <freewayrong@foxmail.com>
2024-11-20 01:53:12 +0800

    Feature: Add application process duplication detection

--- Avalonia.CpuLimiter/Program.cs
+++ Avalonia.CpuLimiter/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using Avalonia.CpuLimiter.Models;
 using Avalonia.Logging;
@@ -15,17 +16,26 @@ namespace Avalonia.CpuLimiter
 {
     internal sealed class Program
     {
+        private static FileStream _fileLock;
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
         public static void Main(string[] args)
         {
+            if (!CheckProcessUnique())
+            {
+                Console.WriteLine("duplicate process has already been started");
+                Console.WriteLine("Exiting...");
+                return;
+            }
+
 
             if(!AdminRunner.IsRunAsAdmin())
             {
                 Console.WriteLine("is not run as admin");
                 Console.WriteLine("try run as admin");
+                Console.WriteLine($@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}");
                 // AdminRunner.RunElevated();
                 // Environment.Exit(0);
             }
@@ -53,6 +63,46 @@ namespace Avalonia.CpuLimiter
                 })                
                 .LogToTrace(LogEventLevel.Information)
                 .UseReactiveUI();
+
+        private static bool CheckProcessUnique()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // add a mutex to synchronize between multi processes to stop repeat launch
+                bool mutexCreated;
+                const string AppName = "Avalonia.CpuLimiter";
+
+                var mutex = new Mutex(true, AppName, out mutexCreated);
+                if (!mutexCreated)
+                {
+                    Console.WriteLine("You can only start one instance of Avalonia.CpuLimiter.");
+                    return false;
+                }
+                Console.WriteLine("This is a unique instance of the Avalonia.CpuLimiter.");
+                return true;
+            }
+            else
+            {
+                string lockPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AboutInfo.AppName,
+                    ".lock");
+
+                if (!Directory.Exists(Path.GetDirectoryName(lockPath)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(lockPath));
+                }
+
+                try
+                {
+                    _fileLock = File.Open(lockPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+                    _fileLock.Lock(0,0);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            } 
+        }
     }
 }
 
